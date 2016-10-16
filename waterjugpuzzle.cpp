1a1c5fe93aff64956bcca15a2b2339fb111e49b6
Matt Rota <mrota@stevens.edu>
2016-10-16 19:48:07 -0400

    commenting bullshit

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -76,57 +76,6 @@ bool argChecks(int argc, char *argv[]) {
 	return false;
 }
 
-
-/*
-int argChecks(int argcount, char *args[]){ //argument checking
-	// had to use multiple for loops over the same exact parameters bc the way the
-	// test cases were set up and it's a HUGE pain and it hurts me to have done so
-	// but it WORKS
-	if (argcount != 7){ //incorrect number of arguments
-		cerr << "Usage:" << args[0] << "<cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
-		return 1;
-	}
-	istringstream iss;
-	int m;
-	char jugs[3] = {'A', 'B', 'C'};
-	for (int i = 1; i < argcount; i++){ // arguments aren't integers
-		iss.str(args[i]);
-		if(!(iss >> m)){
-			if (i <=3){
-				cerr << "Error: Invalid capacity '" << args[i] << "' for jug " << jugs[i-1] << "." << endl;
-				return 1;
-			} else {
-				cerr << "Error: Invalid goal '" << args[i] << "' for jug " << jugs[i%4] << "." << endl;
-				return 1;
-			};
-		};
-		iss.clear();
-	};
-	for(int i = 1; i < argcount; i++){ // arguments are integers, but bad user input
-		if (atoi(args[i]) < 0){
-			if (i <=3){
-				cerr << "Error: Invalid capacity '" << args[i] << "' for jug " << jugs[i-1] << "." << endl;
-				return 1;
-			} else {
-				cerr << "Error: Invalid goal '" << args[i] << "' for jug " << jugs[i%4] << "." << endl;
-				return 1;
-			};
-		};
-	};
-	for(int i=1; i < argcount; i++){ // invalid capacity
-		if ((i < 4) && (atoi(args[i+3]) > atoi(args[i]))){
-			cerr << "Error: Goal cannot exceed capacity of jug " << jugs[i-1] <<"." << endl;
-			return 1;
-		};
-	};
-	if (atoi(args[4]) + atoi(args[5]) + atoi(args[6]) != atoi(args[3])){ // invalid goal state
-		cerr << "Error: Total gallons in goal state must be equal to the capacity of jug C." << endl;
-		return 1;
-	}
-	return 0;
-}
-*/
-
 int main(int argc, char *argv[]) {
 	if (argc != 7) {
 		cerr << "Usage: " << argv[0] << " <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
