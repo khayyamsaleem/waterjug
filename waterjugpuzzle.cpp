da89b06d3b9a0f4aacabed125441ade328921331
Matt Rota <mrota@stevens.edu>
2016-10-16 19:06:42 -0400

    argchecks works, will refine

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -19,32 +20,63 @@ struct State {
     }
 };
 
-int atoi( const char *c ) { //converts chars from argv into ints
-    int value = 0; //accumulator
-    int sign = 1;
-    if( *c == '+' || *c == '-' ) {
-       if( *c == '-' ) sign = -1;
-       c++;
-    }
-    while ( isdigit( *c ) ) {
-        value *= 10; //adds worth of digit to value
-        value += (int) (*c-'0'); // casts digit to int
-        c++;
-    }
-    return value * sign;
-}
-
 //void bfs(State initial, State final){
 //
 //}
 
 
+bool argChecks(int argc, char *argv[]) {
+	istringstream iss;
+	int m;
+	char jugs[3] = {'A', 'B', 'C'};
+
+	for (int i = 1; i < argc; i++) {
+		iss.clear();
+		iss.str(argv[i]);
+		if (!(iss >> m)) {
+			if (i <= 3) {
+				cerr << "Error: Invalid capacity '" << argv[i] << "' for jug " << jugs[i-1] << "." << endl;
+				return true;
+			}
+			else {
+				cerr << "Error: Invalid goal '" << argv[i] << "' for jug " << jugs[i%4] << "." << endl;
+				return true;
+			}
+		}
+		else if (m < 1) {
+			if (i <= 3 && m < 1) {
+				cerr << "Error: Invalid capacity '" << argv[i] << "' for jug " << jugs[i-1] << "." << endl;
+				return true;
+			}
+			else {
+				if (m != 0) {
+					cerr << "Error: Invalid goal '" << argv[i] << "' for jug " << jugs[i%4] << "." << endl;
+					return true;
+				}
+			}
+		}
+	}
+	for (int i = 1; i < argc; i++) {
+		if ((i < 4) && (atoi(argv[i+3]) > atoi(argv[i]))) {
+			cerr << "Error: Goal cannot exceed capacity of jug " << jugs[i-1] << "." << endl;
+			return true;
+		}
+	}
+	if (atoi(argv[4]) + atoi(argv[5]) + atoi(argv[6]) != atoi(argv[3])) {
+		cerr << "Error: Total gallons in goal state must be equal to the capacity of jug C." << endl;
+		return true;
+	}
+	return false;
+}
+
+
+/*
 int argChecks(int argcount, char *args[]){ //argument checking
 	// had to use multiple for loops over the same exact parameters bc the way the
 	// test cases were set up and it's a HUGE pain and it hurts me to have done so
 	// but it WORKS
 	if (argcount != 7){ //incorrect number of arguments
-		cerr << "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+		cerr << "Usage:" << args[0] << "<cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
 		return 1;
 	}
 	istringstream iss;
@@ -86,12 +118,23 @@ int argChecks(int argcount, char *args[]){ //argument checking
 	}
 	return 0;
 }
+*/
 
 int main(int argc, char *argv[]) {
+	if (argc != 7) {
+		cerr << "Usage: " << argv[0] << " <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+		return 1;
+	}
+	if (argChecks(argc, argv)) {
+		return 1;
+	}
 
-	argChecks(argc, argv);
-	State i(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
-	State f(atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+	return 0;
+
+
+//	argChecks(argc, argv);
+//	State i(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+//	State f(atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
 //	State s(0, 0, 8);
 //	cout << s.to_string() << endl;
 //	s.a += 3;
